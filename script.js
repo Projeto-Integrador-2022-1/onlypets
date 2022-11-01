68262d93786d461f6d5975d7f4671a8e7dcfb9aa
Felipe Moisés <63884810+LipeMoises@users.noreply.github.com>
2022-11-01 16:52:43 -0300

    Atualização 01/11/22
    Reta final, mas eu andei estudando mais sobre javascript
    Não que eu queira implementar no site, entretanto, vai ser útil caso o projeto continue
    
    Basicamente isso

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function myFunction() {
-    var element = document.getElementById("comprar");
-    element.classList.toggle("compraclaro");
- }
+function toDark() {
+    var element = document.getElementById("claro")
+    if (element.classList == "light-mode") {
+        element.classList.replace("light-mode", "dark-mode");
+    }
+
+    else {
+        element.classList.replace("dark-mode", "light-mode")
+    }
+}
