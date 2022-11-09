3aacaaf03b60f9d1391ed1637879c437549c62da
LevySena <marcoslevysena10@gmail.com>
2022-11-09 16:11:14 -0300

    Criado models do pet

--- pet/models.py
+++ pet/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Cadastro_Pet(models.Model):
+    Nome = models.CharField(max_length=300)
+    Peso = models.CharField(max_length=300,blank=True,default='')
+    Nascimento = models.CharField(max_length=300,blank=True,default='')
+    Especie = models.CharField(max_length=300)
+    Sexo = models.CharField(max_length=300)
+    Raça = models.CharField(max_length=300)
+    Pelagem = models.CharField(max_length=300)
+    ImagemPet = models.ImageField('Foto do pet')
