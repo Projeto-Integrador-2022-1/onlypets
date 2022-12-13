f25837c7daf1e9d1607c08430e0233e9a96633ed
LevySena <marcos.levy.sena08@aluno.ifce.edu.br>
2022-12-13 17:46:10 -0300

    Tela de Carrinho W.I.P

--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from veterinario.models import Servico
+from pet.models import Cadastro_Pet
 
 def Carrinho(request,id):
-    
+    pets = Cadastro_Pet.objects.filter(user=request.user.id)
     servicos =Servico.objects.filter(user=id)
     contexto={
+        'pets' : pets,
         "servicos" : servicos
     }
     return render(request,"Carrinho/carrinhovrs2.html",context=contexto)
