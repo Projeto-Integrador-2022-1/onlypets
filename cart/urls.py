28e0e7cd1e44c4c8b99c0c5d12ac450a5460d9f7
LevySena <marcos.levy.sena08@aluno.ifce.edu.br>
2022-12-13 23:20:23 -0300

    Telas ancoradas

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from cart.views import Carrinho,conf_pag,histo
 
+
 urlpatterns = [
 path('<id>/',Carrinho,name='carrinho'),
 path('<id>/pagamento/',conf_pag,name='conf_pag'),
